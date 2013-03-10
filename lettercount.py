153f2ffd90a23efa05995fa26434f17dfbdf9105
Marissa <yaridog@gmail.com>
2013-03-10 15:52:00 -0700

    solution to ex5 countingletters

--- lettercount.py
+++ lettercount.py
@@ -11,19 +11,21 @@ lower_text = txt.lower()
 
 counter = [0]*26
 
-	for x in lower_text:
-		
-#test if character is a letter (97-122)
+for x in lower_text:	
+	#get ord value of the letter
+	intValue = ord(x)
 
-#get value of the letter and subtract 97 to determine the position in the list
-
-#count the letter (increment by 1)
-
-	
+	#test if character is a letter from a-z (97-122)
+	if intValue >= 97 and intValue <= 122:
+	#subtract 97 to determine the position in the list
+		indexNum = intValue - 97
+		count = counter[indexNum]
+		counter[indexNum] = count + 1
 
 
 #print count to screen in alphabetical order
+for x in range(len(counter)):
+	print counter[x]
 
 #pipe contents to 'spark' program
 
-#
